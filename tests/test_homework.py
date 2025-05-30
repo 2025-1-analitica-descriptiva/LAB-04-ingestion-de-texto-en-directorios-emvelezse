70029f0712fee984111ed3c3ef68067f30a3f724
emvelezse <emvelezse@unal.edu.co>
2025-05-29 23:57:37 -0500

    lab4

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -21,6 +21,7 @@ def test_01():
     train_dataset = pd.read_csv("files/output/train_dataset.csv", sep=",")
 
     print(train_dataset.columns)
+    print(train_dataset.shape)
     assert "phrase" in train_dataset.columns
     assert "target" in train_dataset.columns
 
